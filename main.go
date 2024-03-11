17ecdfd9351014c422cdd4040f1b10056a52c3ec
Filip Šedivý <sedivy@apparently.cz>
2024-03-11 15:08:41 +0100

    Vytvoření GET requestu na získání všech dat z databáze

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/go-sql-driver/mysql"
 )
@@ -13,10 +14,11 @@ import (
 type ourHandler struct{}
 
 type Contact struct {
-	ID    int64
-	Name  string
-	Email string
-	Phone uint64
+	ID      int64
+	Name    string
+	Email   string
+	Phone   uint64
+	Created time.Time
 }
 
 func (h ourHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
@@ -33,7 +35,7 @@ func (h ourHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 			panic(err)
 		}
 
-		_, err = addUser(contact)
+		_, err = addContact(contact)
 		if err != nil {
 			panic(err)
 		}
@@ -41,28 +43,34 @@ func (h ourHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	contacts, err := getContactsFromDB()
-	if err != nil {
-		panic(err)
+	if req.Method == "GET" && req.URL.Path == "/contacts" {
+		contacts, err := getContactsFromDB()
+		if err != nil {
+			panic(err)
+		}
+
+		jsonContacts, err := json.Marshal(contacts)
+		if err != nil {
+			panic(err)
+		}
+
+		res.Write(jsonContacts)
+		return
 	}
 
-	json, err := json.Marshal(contacts)
-	if err != nil {
-		panic(err)
-	}
-
-	res.Write(json)
 }
 
 var db *sql.DB
 
-func addUser(contact Contact) (Contact, error) {
-	stmt, err := db.Prepare("INSERT INTO contacts SET name=?, email=?,phone=?")
+func addContact(contact Contact) (Contact, error) {
+	stmt, err := db.Prepare("INSERT INTO contacts SET name=?, email=?, phone=?, created=?")
 	if err != nil {
 		return Contact{}, err
 	}
 
-	result, err := stmt.Exec(contact.Name, contact.Email, contact.Phone)
+	created := time.Now().Format("2006-01-02")
+
+	result, err := stmt.Exec(contact.Name, contact.Email, contact.Phone, created)
 	if err != nil {
 		return Contact{}, err
 	}
@@ -75,22 +83,23 @@ func addUser(contact Contact) (Contact, error) {
 	return contact, nil
 }
 
-func getContactById(contactID int64) (Contact, error) {
+/*
+func getContactById(contactID Contact) (Contact, error) {
 
-	query := "SELECT * FROM contacts WHERE id =?"
+		query := "SELECT * FROM contacts WHERE id =?"
 
-	row := db.QueryRow(query, contactID)
+		row := db.QueryRow(query, contactID)
 
-	var contact Contact
+		var contact Contact
 
-	err := row.Scan(&contact.ID, &contact.Name, &contact.Email, &contact.Phone)
-	if err != nil {
-		return Contact{}, err
+		err := row.Scan(&contact.ID, &contact.Name, &contact.Email, &contact.Phone)
+		if err != nil {
+			return Contact{}, err
+		}
+
+		return contact, nil
 	}
-
-	return contact, nil
-}
-
+*/
 func getContactsFromDB() ([]Contact, error) {
 	var contactsFromDB []Contact
 
@@ -99,9 +108,11 @@ func getContactsFromDB() ([]Contact, error) {
 		return nil, err
 	}
 
+	defer rows.Close()
+
 	for rows.Next() {
 		var contact Contact
-		err = rows.Scan(&contact.ID, &contact.Name, &contact.Email)
+		err = rows.Scan(&contact.ID, &contact.Name, &contact.Email, &contact.Phone, &contact.Created)
 		if err != nil {
 			return nil, err
 		}
@@ -114,11 +125,12 @@ func getContactsFromDB() ([]Contact, error) {
 
 func main() {
 	cfg := mysql.Config{
-		User:   "root",
-		Passwd: "local",
-		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-		DBName: "Contact-list",
+		User:      "root",
+		Passwd:    "local",
+		Net:       "tcp",
+		Addr:      "127.0.0.1:3306",
+		DBName:    "Contact-list",
+		ParseTime: true,
 	}
 
 	var err error
